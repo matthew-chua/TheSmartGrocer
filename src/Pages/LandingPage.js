4aad8ae9861f332e5d91f3e01fb35cfcfa896a4a
matthew-chua <matthew@matts-stonks-mac.local>
2022-02-24 11:13:24 +0800

    fonts

--- src/Pages/LandingPage.js
+++ src/Pages/LandingPage.js
@@ -1,7 +1,11 @@
-import React from 'react'
+import React from "react";
+import classes from "./LandingPage.module.css";
 
 export default function LandingPage() {
   return (
-<h1>landing page</h1>
-  )
+    <div className={classes.root}>
+      <h1>landing page</h1>
+      <h2 className={classes.subtitle}>test</h2>
+    </div>
+  );
 }
